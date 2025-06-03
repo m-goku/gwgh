a7d9a95d8c743cace97a28678a89b284b8fae043
goku-ts <hgb.mega@gmail.com>
2025-06-03 23:57:04 +0000

    h

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
           </div>
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Grow Forests. Feed Communities. Change the World.
+              Grow Crops. Feed Communities. Change the World.
             </h1>
             <p className="max-w-2xl mb-6 font-light text-lg text-gray-500 lg:mb-8 md:text-xl lg:text-xl dark:text-gray-400">
               Green World Initiative plants trees and crops to fight
