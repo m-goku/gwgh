a11ba731b7f9bbb5f5a0d3cc6c968a923903866e
goku-ts <hgb.mega@gmail.com>
2025-08-18 17:47:58 +0000

    d

--- app/components/Analytics/Card.tsx
+++ app/components/Analytics/Card.tsx
@@ -10,12 +10,12 @@ export default function Card({ label, value, icon }: CardProps) {
   return (
     <div className="bg-white rounded-2xl shadow-sm p-6 flex justify-between items-center h-[20vh] md:h-[30vh]">
       {/* Left: Image takes 1/3 */}
-      <div className="w-15 h-15 flex items-center justify-center bg-orange-300 rounded-full">
+      <div className="w-20 h-20 ml-5 mr-5 flex items-center justify-center bg-orange-300 rounded-full">
         <img
           src={icon}
           alt={`${label} icon`}
-          width={30}
-          height={30}
+          width={40}
+          height={40}
           className="object-contain"
         />
       </div>
@@ -23,7 +23,7 @@ export default function Card({ label, value, icon }: CardProps) {
       {/* Right: Label + Value stacked (2/3) */}
       <div className="w-2/3 flex flex-col ">
         <h2 className="text-md font-semibold mb-2 text-green-400">{label}</h2>
-        <h1 className="text-2xl font-semibold text-green-900">{value}</h1>
+        <h1 className="text-3xl font-semibold text-green-900">{value}</h1>
       </div>
     </div>
   );
